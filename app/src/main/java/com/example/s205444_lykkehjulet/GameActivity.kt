ac8e691b1b10acde2b0027ea26d2bdb3ceca4dfc
Lucas Skals <skalslucas@gmail.com>
2021-11-18 19:19:03 +0100

    Added spin button to xml files.

--- app/src/main/java/com/example/s205444_lykkehjulet/GameActivity.kt
+++ app/src/main/java/com/example/s205444_lykkehjulet/GameActivity.kt
@@ -5,40 +5,89 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
+import androidx.core.view.children
 import androidx.navigation.fragment.findNavController
-import com.example.s205444_lykkehjulet.databinding.FragmentSecondBinding
 
-/**
- * A simple [Fragment] subclass as the second destination in the navigation.
- */
-class GameActivity : Fragment() {
 
-    private var _binding: FragmentSecondBinding? = null
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
+class GameActivity : AppCompatActivity() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    private val gameManager = GameManager()
 
-        _binding = FragmentSecondBinding.inflate(inflater, container, false)
-        return binding.root
+    private lateinit var wordTextView: TextView
+    private lateinit var lettersUsedTextView: TextView
+    private lateinit var imageView: ImageView
+    private lateinit var gameLostTextView: TextView
+    private lateinit var gameWonTextView: TextView
+    private lateinit var newGameButton: Button
+    private lateinit var lettersLayout: ConstraintLayout
 
-    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_game)
+        imageView = findViewById(R.id.imageView)
+        wordTextView = findViewById(R.id.wordTextView)
+        lettersUsedTextView = findViewById(R.id.lettersUsedTextView)
+        gameLostTextView = findViewById(R.id.gameLostTextView)
+        gameWonTextView = findViewById(R.id.gameWonTextView)
+        newGameButton = findViewById(R.id.newGameButton)
+        lettersLayout = findViewById(R.id.lettersLayout)
+        newGameButton.setOnClickListener {
+            startNewGame()
+        }
+        val gameState = gameManager.startNewGame()
+        updateUI(gameState)
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        binding.buttonSecond.setOnClickListener {
-            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
+        lettersLayout.children.forEach { letterView ->
+            if (letterView is TextView) {
+                letterView.setOnClickListener {
+                    val gameState = gameManager.play((letterView).text[0])
+                    updateUI(gameState)
+                    letterView.visibility = View.GONE
+                }
+            }
         }
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+    private fun updateUI(gameState: GameState) {
+        when (gameState) {
+            is GameState.Lost -> showGameLost(gameState.wordToGuess)
+            is GameState.Running -> {
+                wordTextView.text = gameState.underscoreWord
+                lettersUsedTextView.text = "Letters used: ${gameState.lettersUsed}"
+                imageView.setImageDrawable(ContextCompat.getDrawable(this, gameState.drawable))
+            }
+            is GameState.Won -> showGameWon(gameState.wordToGuess)
+        }
+    }
+
+    private fun showGameLost(wordToGuess: String) {
+        wordTextView.text = wordToGuess
+        gameLostTextView.visibility = View.VISIBLE
+        imageView.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.game7))
+        lettersLayout.visibility = View.GONE
+    }
+
+    private fun showGameWon(wordToGuess: String) {
+        wordTextView.text = wordToGuess
+        gameWonTextView.visibility = View.VISIBLE
+        lettersLayout.visibility = View.GONE
+    }
+
+    private fun startNewGame() {
+        gameLostTextView.visibility = View.GONE
+        gameWonTextView.visibility = View.GONE
+        val gameState = gameManager.startNewGame()
+        lettersLayout.visibility = View.VISIBLE
+        lettersLayout.children.forEach { letterView ->
+            letterView.visibility = View.VISIBLE
+        }
+        updateUI(gameState)
     }
 }
---
+++ app/src/main/java/com/example/s205444_lykkehjulet/GameState.kt
@@ -0,0 +1,10 @@
+package com.example.s205444_lykkehjulet
+
+
+sealed class GameState {
+    class Running(val lettersUsed: String,
+                  val underscoreWord: String,
+                  val drawable: Int) : GameState()
+    class Lost(val wordToGuess: String) : GameState()
+    class Won(val wordToGuess: String) : GameState()
+}
